b34c19c1fac3c5659c90c54a8631e68f6c8fb1c3
Christian Schmidt <cjschmidt33@gmail.com>
2024-12-05 17:32:45 -0500

    Add BarChart to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import { useState } from 'react'
-import ChartComponent from './components/ChartComponent'
-import BarChart
+import { useState, useEffect } from 'react'
+import ChartComponent from '/components/ChartComponent'
+import BarChart from '/components/BarChart';
 import { Bar, Line, Scatter, Bubble } from '/node_modules/react-chartjs-2';
 import './App.css'
 
 function App() {
-  const [chartData, setChartData]
+  const [chartData, setChartData] = useState(null);
+
+  useEffect(() => {
+    fetch('/financial_data.json')
+    .then((response) => response.json())
+    .then((data) => setChartData(data));
+  }, []);
+
+  if (!chartData) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Dynamic Charts with React and Chart.js</h1>
+      <BarChart data={chartData} />
+    </div>
+  );
 }
 
 export default App
