05a617cca2bcfbf0aa618a50da4703dce389c2d3
Christian Schmidt <cjschmidt33@gmail.com>
2024-12-05 17:54:07 -0500

    add line and scatter charts to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react'
 import ChartComponent from './components/ChartComponent'
 import BarChart from './components/BarChart';
+import LineChart from './components/LineChart';
+import ScatterChart from './components/ScatterChart';
 import { Bar, Line, Scatter, Bubble } from '/node_modules/react-chartjs-2';
 import './App.css'
 
@@ -21,6 +23,8 @@ function App() {
     <div style={{ textAlign: 'center' }}>
       <h1>Dynamic Charts with React and Chart.js</h1>
       <BarChart data={chartData} />
+      <LineChart data={chartData} />
+      <ScatterChart data={chartData} />
     </div>
   );
 }
